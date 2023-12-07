0de8a6346ee5f17573302c7378d33f5aa6716d35
shuyu0714 <upgedc13@hotmail.com>
2023-12-07 17:45:10 +0800

    project files init

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/2023F2E_01/",
+  plugins: [react()],
+})
