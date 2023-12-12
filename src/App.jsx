cb2f19cbc0a6cccee3e49f1191f2fd4f55c6695a
shuyu0714 <upgedc13@hotmail.com>
2023-12-12 17:15:15 +0800

    CSS Gideline Setting

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ import './App.css'
 
 function App() {
 
-  return (
-    <></>
-  )
+  return <h1 className="text-head font-bold underline bg-primary-5 font-serif hover:bg-primary-3">Hello world!鑑於對人類家庭所有成員的固有尊嚴及其平等的和不移的權利的承認，乃是世界自由、正義與和平的基礎</h1>;
 }
 
 export default App
---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      serif: ['"Noto Serif TC"'],
+    },
+    fontSize: {
+      'head': '48px',
+      'title': {
+        1: '28px',
+        2: '24px'
+      },
+      'subtitle': '20px',
+      'body': '16px'
+    },
+    extend: {
+      colors: {
+        'primary': {
+          1: '#FBFFE1',
+          2: '#EEFFA9',
+          3: '#D1FA31',
+          4: '#BBE217',
+          5: '#A8C400',
+        },
+        'grayScale': {
+          1: '#E2E2E2',
+          2: '#C6C6C6',
+          3: '#8C8C8C',
+          4: '#444444',
+          5: '#000000',
+        },
+      }
+    },
+  },
+  plugins: [],
+}
+
